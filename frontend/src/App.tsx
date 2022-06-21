85d5733f73405253a3b633e5111a199cc68fcd1a
ecam900 <ecam900@gmail.com>
2022-06-20 23:41:38 -0400

    initial commit.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import logo from './assets/images/logo-universal.png';
+import './App.css';
+import { Greet } from '../wailsjs/go/main/App';
+
+function App() {
+  const [resultText, setResultText] = useState(
+    'Please enter your name below 👇'
+  );
+  const [name, setName] = useState('');
+  const updateName = (e: any) => setName(e.target.value);
+  const updateResultText = (result: string) => setResultText(result);
+
+  async function greet() {
+    const r = await Greet(name);
+    updateResultText(r);
+  }
+
+  return (
+    <div id='App'>
+      <img src={logo} id='logo' alt='logo' />
+      <div id='result' className='result'>
+        {resultText}
+      </div>
+      <div id='input' className='input-box'>
+        <input
+          id='name'
+          className='input'
+          onChange={updateName}
+          autoComplete='off'
+          name='input'
+          type='text'
+        />
+        <button className='btn' onClick={greet}>
+          Greet
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
